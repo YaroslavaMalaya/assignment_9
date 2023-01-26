1263b4e3857064d9c9576b2ee2609f83acf1e3eb
Yaroslava Mala <ymala@kse.org.ua>
2023-01-27 01:23:04 +0200

    task 1; task 2

--- main.py
+++ main.py
@@ -2,11 +2,55 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# data1 = pd.read_csv('titles.csv')
-# data1.groupby('type')['imdb_score'].count().plot.pie()
+titles = pd.read_csv('titles.csv')
+credits = pd.read_csv('credits.csv')
+# titles.groupby('type')['imdb_score'].count().plot.pie()
+# titles.info()
+# credits.info()
 
-names = ["movie", "show"]
-values = [1,20]
-plt.bar(names, values)
-plt.show()
 
+def first():
+    movie_scores = titles[titles["type"] == "MOVIE"]["imdb_score"].dropna()
+    show_scores = titles[titles["type"] == "SHOW"]["imdb_score"].dropna()
+    plt.hist(movie_scores, np.arange(0, 10.2, 0.2))
+    plt.hist(show_scores, np.arange(0, 10.2, 0.2))
+    plt.title('Histogram of movie and show')
+    plt.xlabel("imdb_score")
+    plt.ylabel("Number of titles")
+    print(titles.groupby(by=["type"])["imdb_score"].mean())
+    plt.axvline(movie_scores.mean(), color="k", linestyle="dashed", linewidth=1)
+    plt.axvline(show_scores.mean(), color="r", linestyle="dashed", linewidth=1)
+    plt.legend(('average film score', 'average show score', 'movie', 'show' ),
+           loc='upper left')
+    plt.grid()
+    plt.show()
+
+
+def first1():  # інший варіант побудови гістограм
+    bins = np.arange(0, 10.2, 0.2)
+    hists = titles.hist(column="imdb_score", by="type", bins=bins, edgecolor="black", xlabelsize=5)
+    for hist in hists:
+        hist.set_xticks(bins)
+        hist.set_xlabel("imdb_score")
+        hist.set_ylabel("Number of titles")
+        # hist.axvline((hist["type"])["imdb_score"].mean(), color="red", linestyle="dashed", linewidth=2)
+        hist.grid()
+    print(titles.groupby(by=["type"])["imdb_score"].mean())
+    plt.show()
+
+
+def second():
+    values = titles[titles["type"] == "SHOW"]["age_certification"].dropna()
+    labels, values = np.unique(values, return_counts=True)
+    colors = ['yellowgreen', 'blue', 'orange', 'yellow', 'crimson', 'purple']
+    plt.pie(values, colors=colors, labels=labels, autopct='%1.1f%%')
+    plt.show()
+
+
+def third():
+    pass
+
+
+first()
+second()
+# first1()
